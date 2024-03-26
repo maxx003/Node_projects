f2f81290b42e610e5ab26f1c8fcf4d25ff2a253d
Theres Francis <theresmariafrancis@gmail.com>
2024-03-26 10:03:00 -0600

    My Second Commit

--- app.js
+++ app.js
@@ -1,33 +1,23 @@
 const express = require("express");
 const ejs = require("ejs");
+const fs = require("fs").promises;
+require("dotenv").config();
+const userRoute = require('./routes/users.js');
 const app = express();
 
-const port = 3000;
 
-app.get("/", (req, res) => {
-  const id = req.query.id;
-  const username = req.query.username;
 
-  res.send(`User ID: ${id}, Username: ${username}`);
-});
+app.use(userRoute);
+app.use(express.static('public'));
 
-app.get("/products/:id", (req, res) => {
-  const productId = req.params.id;
+app.set('view engine','ejs');
+app.set('views', './views');
 
-  const products = [
-    { id: 1, name: "Product A" },
-    { id: 2, name: "Product B" },
-    { id: 3, name: "Product C" },
-  ];
 
-  const product = products.find(
-    (product) => product.id === parseInt(productId)
-  );
 
-  product
-    ? res.send(`Product id: ${product.id}. Name: ${product.name}`)
-    : res.status(404).send("Product not Found.");
-});
+
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
